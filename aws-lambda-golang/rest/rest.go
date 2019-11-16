5dcdd098fba684c926d10d10678c3d35689d94df
Chris Compton <christopher.j.compton@gmail.com>
2019-11-16 14:40:44 -0500

    Check httpMethod and perform API call

--- aws-lambda-golang/rest/rest.go
+++ aws-lambda-golang/rest/rest.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -10,18 +11,28 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-const PATH = "/.netlify/functions/rest"
-
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 
-	var returnBody string
-	var returnCode int
+	const PATH = "/.netlify/functions/rest"
+	var returnBody string = ""
+	var returnCode int = -1
+
+	if request.HTTPMethod != http.MethodGet {
+		returnCode = 405
+		returnBody = "Method not allowed: " + request.HTTPMethod
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
+		}, nil
+	}
+
 	api_url, present := os.LookupEnv("API_URL")
 	if present == false {
 		returnCode = 500
 		returnBody = "Server error, missing environment url"
-		return &events.APIGatewayProxyResponse{StatusCode: returnCode,
-			Body: returnBody,
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
 		}, nil
 	}
 
@@ -29,19 +40,26 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	if present == false {
 		returnCode = 500
 		returnBody = "Server error, missing api key"
-		return &events.APIGatewayProxyResponse{StatusCode: returnCode,
-			Body: returnBody,
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
 		}, nil
 	}
 
+	log.Println("API_URL: " + api_url)
+	log.Println("API_KEY: " + api_key)
+
 	requestUrl := strings.Replace(request.Path, PATH, api_url, -1)
+
+	log.Println("Request URL: " + requestUrl)
 
 	myRequest, err := http.NewRequest(http.MethodGet, requestUrl, nil)
 	if err != nil {
 		returnCode = 500
 		returnBody = "Server error creating new request"
-		return &events.APIGatewayProxyResponse{StatusCode: returnCode,
-			Body: returnBody,
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
 		}, err
 	}
 
@@ -49,23 +67,26 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 
 	client := &http.Client{}
 
+	log.Println(client)
 	resp, err := client.Do(myRequest)
 	if err != nil {
 		returnCode = resp.StatusCode
 		returnBody = resp.Body.Close().Error()
-		return &events.APIGatewayProxyResponse{StatusCode: returnCode,
-			Body: returnBody,
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
 		}, err
 	}
+	defer resp.Body.Close()
 
 	returnCode = resp.StatusCode
-	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 
-	returnBody = string(body[len(body)])
+	returnBody = string(body[:])
 
-	return &events.APIGatewayProxyResponse{StatusCode: returnCode,
-		Body: returnBody,
+	return &events.APIGatewayProxyResponse{
+		StatusCode: returnCode,
+		Body:       returnBody,
 	}, nil
 }
 

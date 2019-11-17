a36bb6a945dd6979ab02dbdbe621e358431cbfb2
C-Compton <C-Compton@users.noreply.github.com>
2019-11-16 23:36:12 -0500

    Dynamically generate REST API GET request. (#4)
    * Test response values actually change.
    
    * WIP: Perform rest api call
    
    * Check httpMethod and perform API call
    
    * Put logging back
    
    * Parse query string parameters to add to request url
    
    * Accept application/json
    
    * Content-type application/json in response header
    
    * Remove excess logging. Log errors more accurately.

--- aws-lambda-golang/rest/rest.go
+++ aws-lambda-golang/rest/rest.go
@@ -1,66 +1,125 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/events"
-        "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 
-	log.Print("request.Resource")
-	log.Printf("    %s\n", request.Resource)
+	const PATH = "/.netlify/functions/rest"
+	var returnBody string
+	var returnCode int
+
+	if request.HTTPMethod != http.MethodGet {
+		returnCode = 405
+		returnBody = "Method not allowed: " + request.HTTPMethod
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
+		}, nil
+	}
+
 	log.Print("request.Path")
 	log.Printf("    %s\n", request.Path)
-	log.Print("request.HTTPMethod")
-	log.Printf("    %s\n", request.HTTPMethod)
-
-	log.Print("request.Headers")
-	for key, value := range request.Headers {
-		log.Printf("    %s: %s\n", key, value)
-	}
-
-	log.Printf("request.MulltiValueHeaders")
-	for key, values := range request.MultiValueHeaders {
-		log.Printf("    key: %s [\n", key)
-		for value := range values {
-			log.Printf("        %s\n", value)
-		}
-	}
 
 	log.Print("request.QueryStringParameters")
 	for key, value := range request.QueryStringParameters {
 		log.Printf("    %s: %s\n", key, value)
 	}
 
-	log.Printf("    %s\n", request.MultiValueQueryStringParameters)
-	for key, values := range request.MultiValueQueryStringParameters {
-		log.Printf("    key: %s [\n", key)
-		for value := range values {
-			log.Printf("        %s\n", value)
+	api_url, present := os.LookupEnv("API_URL")
+	if present == false {
+		returnCode = 500
+		returnBody = "Server side error"
+		log.Fatal("Missing API_URL environment variable")
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
+		}, nil
+	}
+
+	api_key, present := os.LookupEnv("API_KEY")
+	if present == false {
+		returnCode = 500
+		returnBody = "Server side error"
+		log.Fatal("Missing API_KEY environment variable")
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
+		}, nil
+	}
+
+	requestUrl := strings.Replace(request.Path, PATH, api_url, -1)
+
+	i := 0
+	l := len(request.QueryStringParameters)
+	if l > 0 {
+		requestUrl = requestUrl + "?"
+		for key, value := range request.QueryStringParameters {
+			requestUrl = requestUrl + key + "=" + value
+			i++
+			if i < l {
+				requestUrl += "&"
+			}
 		}
 	}
 
-	log.Print("request.PathParameters")
-	for key, value := range request.PathParameters {
-		log.Printf("    %s: %s\n", key, value)
-	}
-	
-	log.Print("request.StageVariables")
-	for key, value := range request.StageVariables {
-		log.Printf("    %s: %s\n", key, value)
-	}
-	
-	log.Print("request.Body")
-	log.Printf("    %s\n", request.Body)
+	log.Println("Request URL: " + requestUrl)
 
-	log.Print("IsBase64Encoded")
-	log.Printf("    %t\n", request.IsBase64Encoded)
+	myRequest, err := http.NewRequest(http.MethodGet, requestUrl, nil)
+	if err != nil {
+		returnCode = 500
+		returnBody = "Server side error"
+		log.Fatal("Failed to generate request object")
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
+		}, err
+	}
 
-	return &events.APIGatewayProxyResponse{StatusCode: 200,
-		Headers: request.Headers,
-		MultiValueHeaders: request.MultiValueHeaders,
-		Body: request.Body,
+	myRequest.Header.Add("Authorization", api_key)
+	myRequest.Header.Add("Accept", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(myRequest)
+	if err != nil {
+		returnCode = resp.StatusCode
+		returnBody = resp.Body.Close().Error()
+		log.Println("Error reaching " + requestUrl)
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
+		}, err
+	}
+	defer resp.Body.Close()
+
+	returnCode = resp.StatusCode
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		returnCode = 500
+		returnBody = "Server side error"
+		log.Fatal("Failed to read response body")
+		return &events.APIGatewayProxyResponse{
+			StatusCode: returnCode,
+			Body:       returnBody,
+		}, err
+	}
+
+	returnBody = string(body[:])
+	returnHeader := make(map[string]string)
+	returnHeader["Content-type"] = "application/json"
+	return &events.APIGatewayProxyResponse{
+		StatusCode: returnCode,
+		Headers:    returnHeader,
+		Body:       returnBody,
 	}, nil
 }
 
